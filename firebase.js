26d821217e4ff02ad32de5c8b22c5cc4f722fba6
kmitaebdata-ux <kmitaebdata@gmail.com>
2025-11-29 11:34:11 +0530

    firebase.js
    Updated the authDomain and storageBucket in firebaseConfig.

--- firebase.js
+++ firebase.js
@@ -4,14 +4,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase
 
 export const firebaseConfig = {
   apiKey: "AIzaSyC1Aa_mnq_0g7ZEuLbYVjN62iCMWemlKUc",
-  authDomain: "kmit-marks-portal-9db76.firebaseapp.com",
+  authDomain: "kmitmarks.vercel.app",
   projectId: "kmit-marks-portal-9db76",
-  storageBucket: "kmit-marks-portal-9db76.firebasestorage.app",
+  storageBucket: "kmit-marks-portal-9db76.appspot.com",
   messagingSenderId: "264909025742",
   appId: "1:264909025742:web:84de5216860219e6bc3b9f",
   measurementId: "G-JMZ564P8PJ"
 };
 
+
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
